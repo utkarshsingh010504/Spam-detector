997fa76f2d9569f97e5c9794304d76220af17577
utkarshsingh010504 <utkarshsingh010504@gmail.com>
2026-02-23 21:11:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import string
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
-
+nltk.download('punkt', quiet=True)
+nltk.download('punkt_tab', quiet=True)
+nltk.download('stopwords', quiet=True)
 ps = PorterStemmer()
 
 
